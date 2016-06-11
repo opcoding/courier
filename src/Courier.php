df4c3945d3d136b67e1f65fe4e86e918280eadd6
Gauthier Delamarre <gauthier@objective-php.org>
2016-06-11 12:07:30 +0200

    Update Courier

---
+++ src/Courier.php
@@ -0,0 +1,42 @@
+<?php
+
+    
+    namespace OpCoding\Courier;
+    
+    
+    use PHPCI\Builder;
+    use PHPCI\Model\Build;
+    use PHPCI\Plugin;
+
+    class Courier implements Plugin
+    {
+
+        /**
+         * @var string
+         */
+        protected $directory;
+
+
+        /**
+         * @var Builder
+         */
+        protected $phpci;
+
+        /**
+         * @var Build
+         */
+        protected $build;
+
+        public function __construct(Builder $phpci, Build $build, array $options = [])
+        {
+            $path            = $phpci->buildPath;
+            $this->phpci     = $phpci;
+            $this->build     = $build;
+            $this->directory = $path;
+        }
+
+        public function execute()
+        {
+            var_dump($this->phpci);
+        }
+    }
